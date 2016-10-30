c9271f72815fc4a36b42d0c98c1b7b594204dcaf
migszxc <migszxc@gmail.com>
2016-10-30 18:52:25 +0800

    Now able to read a .txt file and put it into the board[][] array :)

--- src/snakesAndLaddersMS.c
+++ src/snakesAndLaddersMS.c
@@ -8,14 +8,13 @@ typedef struct {
   will be denoted by "H#", "T#", "B#", and "A#", respectively. The # will
   be replaced by a unique number to differ each snakeHead from each other.
   There can only be a maximum of two static elements per cell. */
-  char players[4][2];
+  char players[2][2];
   /* players - a multidimensional array that will contain the players of a
-  cell. These will be denoted by "P#" with the # being replaced with which
-  player number is in the cell. There can be a maximum of 4 players in a
-  cell. (You can only have 4 players in the game)*/
+  cell. These will be denoted by a symbol. There can be a maximum of 4 players
+  in a cell. (You can only have 4 players in the game.)*/
 }cell;
 
-void printBoard(cell board[]);
+void printBoard(cell board[][8]);
 
 int main() {
   // declarations and initializations:
@@ -29,21 +28,58 @@ int main() {
   FILE *fp;
   fp = fopen("rip.txt", "r");
   // every two lines in the .txt file is a row
-  for (i = 0; i < 8; i += 1) {
-     c = fgetc(fp);
-     if ( c == '|') {
-       temp[0] = fgetc(fp);
-       temp[1] = fgetc(fp);
-       board[0][i].staticElements[0][0] = temp[0];
-       board[0][i].staticElements[0][1] = temp[1];
+  for (j = 0; j < 8; j += 1) {
+    for (i = 0; i < 8; i += 1) {
+      c = fgetc(fp);
+      if ( c == '|') {
+        temp[0] = fgetc(fp);
+        temp[1] = fgetc(fp);
+        board[j][i].staticElements[0][0] = temp[0];
+        board[j][i].staticElements[0][1] = temp[1];
+      } else {
+        i -= 1;
+        c = fgetc(fp);
+      }
     }
-
+    for (i = 0; i < 8; i += 1) {
+      c = fgetc(fp);
+      if ( c == '|') {
+        temp[0] = fgetc(fp);
+        temp[1] = fgetc(fp);
+        board[j][i].staticElements[1][0] = temp[0];
+        board[j][i].staticElements[1][1] = temp[1];
+      } else {
+        i -= 1;
+        c = fgetc(fp);
+      }
+    }
   }
-
+  // printf("wtf");
+  printBoard(board);
   fclose(fp);
 }
 
 void printBoard(cell board[][8]) {
-  int i;
-
+  int i, j;
+  char temp[2];
+  printf("   ");
+  for (i = 0; i < 8; i += 1) {
+    printf("|c%d", i);
+  }
+  printf("|\n");
+  for (j = 0; j < 8; j += 1) {
+    printf("|r%d", j);
+    for (i = 0; i < 8; i += 1) {
+      temp[0] = board[j][i].staticElements[0][0];
+      temp[1] = board[j][i].staticElements[0][1];
+      printf("|%c%c",temp[0],temp[1]);
+    }
+    printf("|\n   ");
+    for (i = 0; i < 8; i += 1) {
+      temp[0] = board[j][i].staticElements[1][0];
+      temp[1] = board[j][i].staticElements[1][1];
+      printf("|%c%c",temp[0],temp[1]);
+    }
+    printf("|\n");
+  }
 }
