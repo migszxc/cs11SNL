26d276c3f334bc934d51b0c19bcefbd36e9f11c8
migszxc <migszxc@gmail.com>
2016-10-30 18:03:52 +0800

    starting to make the board

--- src/snakesAndLaddersMS.c
+++ src/snakesAndLaddersMS.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 typedef struct {
-  char staticElements[2][3];
+  char staticElements[2][2];
   /* staticElements - a multidimensional array that will contain the non-moving
   elements of a cell such as snake heads/tails and ladder bottom/tops. These
   will be denoted by "H#", "T#", "B#", and "A#", respectively. The # will
-  be replaced by a unique number to differ each snakeHead from each other. */
-  char players[2][2];
+  be replaced by a unique number to differ each snakeHead from each other.
+  There can only be a maximum of two static elements per cell. */
+  char players[4][2];
   /* players - a multidimensional array that will contain the players of a
   cell. These will be denoted by "P#" with the # being replaced with which
   player number is in the cell. There can be a maximum of 4 players in a
@@ -21,21 +22,28 @@ int main() {
   cell board[8][8];
   // board - an 8 x 8 multidimensional array made of datatype: "cell".
   // read board.txt file and fill up 'board' with the data gathered.
-  int i, j;
-  // i - counter pour for loop
+  int i, j, k;
+  // counters pour for loop
+  char c, temp[2];
+  // char siu
   FILE *fp;
-  fp = fopen("board.txt", "r");
-  // if the file does not exist, create a new one.
-  if ( ferror(fp) ) {
-    printf("\"board.txt\" does not exist, creating a new one.\n");
-    fclose(fp);
-    fp = fopen("board.txt","w");
-    for (j = 0; j < 8; j += 1) {
-      for (i = 0; i < 8; i += 1) {
-        fprintf(fp, "|  ");
-      }
-      fprintf(fp, "|\n");
+  fp = fopen("rip.txt", "r");
+  // every two lines in the .txt file is a row
+  for (i = 0; i < 8; i += 1) {
+     c = fgetc(fp);
+     if ( c == '|') {
+       temp[0] = fgetc(fp);
+       temp[1] = fgetc(fp);
+       board[0][i].staticElements[0][0] = temp[0];
+       board[0][i].staticElements[0][1] = temp[1];
     }
+
   }
+
   fclose(fp);
+}
+
+void printBoard(cell board[][8]) {
+  int i;
+
 }
