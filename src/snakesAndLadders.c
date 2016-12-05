d66ba02253d5b5df2522b0d4a50a77a4a8a9d713
migszxc <migszxc@gmail.com>
2016-12-05 23:19:54 +0800

    Menu is finished
    Additional features include:
    reloading the board.txt file in program without having to restart
    the ability to change and pick characters
    changing the game mode from automatic to manual and vice versa
    exiting
    and it looks pretty
    ascii art
    woo
    :)

--- src/snakesAndLadders.c
+++ src/snakesAndLadders.c
@@ -42,6 +42,8 @@ int checkPlayer(cell board[][8], playah *dude);
 int checkWin(cell board[][8], playah *dude, int *winz);
 void playerTurn(cell board[][8], playah *p1, char plyr, int *winners);
 void printPlacement(playah dude);
+void readBoard(cell board[][8]);
+void changeCharacter(char *plyr);
 
 int main() {
   srand(time(NULL));
@@ -66,10 +68,13 @@ int main() {
   // setting each player's position to be on the bottom left of the board
   cell board[8][8];
   // board - an 8 x 8 multidimensional array of datatype: "cell".
-  int i, j, k, l, r, state, winners;
+  int i, j, k, l, r, state, winners, choice, mode;
+  choice = 0;
+  mode = 0;
   winners = 0;
   // counters for for loops
   char c, temp[2], plyr, pause;
+  plyr = '$';
   /* go through each cell in the board and initialize staticElements
    and players with ' ' */
   for (j = 0; j < 8; j += 1) {
@@ -87,6 +92,177 @@ int main() {
     }
   }
   // open 'board.txt' and fill up the cell array 'board' with the information
+  readBoard(board);
+  /*set the bottom left corner to be the starting point and then place all the
+  players  there */
+  while (choice != 5) {
+    system("cls");
+    printf("   ____          __                          __  __           __   __          \n");
+    printf("  / __/__  ___ _/ /_____ ___   ___ ____  ___/ / / /  ___ ____/ /__/ /__ _______\n");
+    printf(" _\\ \\/ _ \\/ _ `/  '_/ -_|_-<  / _ `/ _ \\/ _  / / /__/ _ `/ _  / _  / -_) __(_-<\n");
+    printf("/___/_//_/\\_,_/_/\\_\\\\__/___/  \\_,_/_//_/\\_,_/ /____/\\_,_/\\_,_/\\_,_/\\__/_/ /___/\n\n");
+    printf("\t\t\tVersion 4.07\t\tGoSanchezSy\n\n");
+    printf("\t\t\t\t1> Start Game\n");
+    printf("\t\t\t\t2> Reload board.txt\n");
+    printf("\t\t\t\t3> Change Character\n");
+    if (mode == 0) {
+      printf("\t\t\t\t4> Mode: Manual\n");
+    } else if (mode == 1) {
+      printf("\t\t\t\t4> Mode: Automatic\n");
+    }
+    printf("\t\t\t\t5> Exit\n\n");
+    printf("\t\t\tChoice: ");
+    scanf("%d", &choice);
+
+    switch(choice) {
+      case 1:
+      readBoard(board);
+      winners = 0;
+      p1.x = 0; p1.y = 7;
+      p1.px = 0; p1.py = 0;
+      p1.place = 0;
+      p1.player = 1;
+      p2.x = 0; p2.y = 7;
+      p2.px = 0; p2.py = 1;
+      p2.place = 0;
+      p2.player = 2;
+      p3.x = 0; p3.y = 7;
+      p3.px = 1; p3.py = 0;
+      p3.place = 0;
+      p3.player = 3;
+      p4.x = 0; p4.y = 7;
+      p4.px = 1; p4.py = 1;
+      p4.place = 0;
+      p4.player = 4;
+      board[7][0].players[0][0] = plyr;
+      board[7][0].players[1][0] = (char)238;
+      board[7][0].players[0][1] = (char)239;
+      board[7][0].players[1][1] = (char)229;
+      printBoard(board, plyr);
+      printf("Press enter to start the game...\n");
+      pause = getchar();
+      pause = getchar();
+      // game starts here
+      while (winners < 3) {
+        playerTurn(board, &p1, plyr, &winners);
+        if (mode == 0) {
+          printf("Press enter to continue...\n");
+          pause = getchar();
+        } else {
+          Sleep(1000);
+        }
+        playerTurn(board, &p2, plyr, &winners);
+        if (mode == 0) {
+          printf("Press enter to continue...\n");
+          pause = getchar();
+        } else {
+          Sleep(1000);
+        }
+        playerTurn(board, &p3, plyr, &winners);
+        if (mode == 0) {
+          printf("Press enter to continue...\n");
+          pause = getchar();
+        } else {
+          Sleep(1000);
+        }
+        playerTurn(board, &p4, plyr, &winners);
+        if (mode == 0) {
+          printf("Press enter to continue...\n");
+          pause = getchar();
+        } else {
+          Sleep(1000);
+        }
+      }
+      printBoard(board, plyr);
+      printf("\n");
+      printPlacement(p1);
+      printPlacement(p2);
+      printPlacement(p3);
+      printPlacement(p4);
+      printf("Press enter to go back to main menu...\n");
+      pause = getchar();
+      // game ends here
+      break;
+      case 2:
+      readBoard(board);
+      break;
+      case 3:
+      changeCharacter(&plyr);
+      break;
+      case 4:
+      if (mode == 0)  { mode = 1; }
+      else if (mode == 1) { mode = 0; }
+      break;
+      default:
+      break;
+    }
+  }
+}
+
+/* function to initialize the character selection screen*/
+void changeCharacter(char *plyr) {
+  int choice;
+  for (;;) {
+    choice = 0;
+    system("cls");
+    printf("   ____          __                          __  __           __   __          \n");
+    printf("  / __/__  ___ _/ /_____ ___   ___ ____  ___/ / / /  ___ ____/ /__/ /__ _______\n");
+    printf(" _\\ \\/ _ \\/ _ `/  '_/ -_|_-<  / _ `/ _ \\/ _  / / /__/ _ `/ _  / _  / -_) __(_-<\n");
+    printf("/___/_//_/\\_,_/_/\\_\\\\__/___/  \\_,_/_//_/\\_,_/ /____/\\_,_/\\_,_/\\_,_/\\__/_/ /___/\n\n");
+    printf("\t\tCharacter Selection: \n\n");
+    printf("\t\t\t1>%c\t2>%c\t3>%c\n\n",232,233,234);
+    printf("\t\t\t4>%c\t5>%c\t6>%c\n\n",235,236,237);
+    printf("\t\tChoice: ");
+    scanf("%d",&choice);
+
+    switch(choice) {
+      case 1:
+      *plyr = (char)232;
+      break;
+      case 2:
+      *plyr = (char)233;
+      break;
+      case 3:
+      *plyr = (char)234;
+      break;
+      case 4:
+      *plyr = (char)235;
+      break;
+      case 5:
+      *plyr = (char)236;
+      break;
+      case 6:
+      *plyr = (char)237;
+      break;
+      default:
+      break;
+    }
+    if (choice > 0 && choice < 7) {
+      break;
+    }
+  }
+
+}
+
+/* function to read board.txt and update the board in-program*/
+void readBoard(cell board[][8]) {
+  int i, j, k, l;
+  char c, temp[2];
+
+  for (j = 0; j < 8; j += 1) {
+    for (i = 0; i < 8; i += 1) {
+      for (k = 0; k < 2; k += 1) {
+        for (l = 0; l < 2; l += 1) {
+          board[j][i].staticElements[k][l] = ' ';
+        }
+      }
+      for (k = 0; k < 2; k += 1) {
+        for (l = 0; l < 2; l += 1) {
+          board[j][i].players[k][l] = ' ';
+        }
+      }
+    }
+  }
   FILE *fp;
   fp = fopen("board.txt", "r");
   // first level loop for each row
@@ -101,7 +277,7 @@ int main() {
         board[j][i].staticElements[0][0] = temp[0];
         board[j][i].staticElements[0][1] = temp[1];
       } else {
-        i -= 1;
+        i-=1;
       }
     }
     // bottom half of each cell
@@ -118,46 +294,6 @@ int main() {
     }
   }
   fclose(fp);
-  // prompt user to pick whatever player logo they want
-  printf("Enter any ASCII character besides  '!', '@', and '#' to be your player token: ");
-  scanf("%c", &plyr);
-  /*set the bottom left corner to be the starting point and then place all the
-  players  there */
-  board[7][0].players[0][0] = plyr;
-  board[7][0].players[1][0] = '@';
-  board[7][0].players[0][1] = '#';
-  board[7][0].players[1][1] = '!';
-  printBoard(board, plyr);
-  printf("Press enter to start the game...\n");
-  pause = getchar();
-  pause = getchar();
-  // game starts here
-  while (winners < 3) {
-    playerTurn(board, &p1, plyr, &winners);
-    printf("Press enter to continue...\n");
-    pause = getchar();
-
-    playerTurn(board, &p2, plyr, &winners);
-    printf("Press enter to continue...\n");
-    pause = getchar();
-
-    playerTurn(board, &p3, plyr, &winners);
-    printf("Press enter to continue...\n");
-    pause = getchar();
-
-    playerTurn(board, &p4, plyr, &winners);
-    printf("Press enter to continue...\n");
-    pause = getchar();
-  }
-  printBoard(board, plyr);
-  printf("\n");
-  printPlacement(p1);
-  printPlacement(p2);
-  printPlacement(p3);
-  printPlacement(p4);
-  printf("Press enter to continue...\n");
-  pause = getchar();
-  // game ends here
 }
 
 /* function that plays a turn for the player only if it hasn't won yet */
@@ -235,21 +371,25 @@ int checkPlayer(cell board[][8], playah *dude) {
     flagged = 1;
     num = board[dude->y][dude->x].staticElements[0][1];
     state = 0;
+    // snake
   }
   if (board[dude->y][dude->x].staticElements[1][0] == 'S') {
     flagged = 1;
     num = board[dude->y][dude->x].staticElements[1][1];
     state = 0;
+    // snake
   }
   if (board[dude->y][dude->x].staticElements[0][0] == 'B') {
     flagged = 2;
     num = board[dude->y][dude->x].staticElements[0][1];
     state = 1;
+    // ladder
   }
   if (board[dude->y][dude->x].staticElements[1][0] == 'B') {
     flagged = 2;
     num = board[dude->y][dude->x].staticElements[1][1];
     state = 1;
+    // ladder
   }
 
   switch(state) {
@@ -438,8 +578,8 @@ void printBoard(cell board[][8], char blah) {
   }
   printf("\n");
   printf("Snake Heads: S#\t\tPlayer 1: %c\n", blah);
-  printf("Snake Tails: T#\t\tPlayer 2: @\n");
-  printf("Ladder Bottoms: B#\tPlayer 3: #\n");
-  printf("Ladder Ends: E#\t\tPlayer 4: !\n");
+  printf("Snake Tails: T#\t\tPlayer 2: %c\n", 238);
+  printf("Ladder Bottoms: B#\tPlayer 3: %c\n", 239);
+  printf("Ladder Ends: E#\t\tPlayer 4: %c\n", 229);
   printf("\n");
 }
